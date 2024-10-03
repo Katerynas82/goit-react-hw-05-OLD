fa3bd4c131f26bc863c35c39dde4f2deeaf07d10
Kateryna Sukovaia <122564463+Katerynas82@users.noreply.github.com>
2024-10-04 00:22:50 +0100

    4

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,17 +1,17 @@
 import { NavLink } from "react-router-dom";
 import s from '../Navigation/Navigation.module.css';
-import clsx from "clsx";
+
 
 
 const Navigation = () => {
-  const buildLinkClass = ({ isActive }) => 
-     clsx(s.link, isActive && s.activeLink);
+  // const buildLinkClass = ({ isActive }) => 
+  //    clsx(s.link, isActive && s.activeLink);
   
   return (
     <div className={s.wrapper}>
       <nav className={s.navHeader}>
-        <NavLink className={buildLinkClass} to="/"> Home</NavLink>
-        <NavLink className={buildLinkClass} to="/movies">Movies</NavLink>
+        <NavLink  to="/"> Home</NavLink>
+        <NavLink  to="/movies">Movies</NavLink>
       </nav>
     </div>
   );
